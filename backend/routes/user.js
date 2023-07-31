56abe6963bb5524f69a92895ee808956d855636d
hokageCodes <hashsenju94@gmail.com>
2023-08-01 00:33:30 +0100

    hOPING

---
+++ backend/routes/user.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const {
+  register,
+  activateAccount,
+  login,
+  auth,
+  sendVerification,
+  findUser,
+  sendResetPasswordCode,
+  validateResetCode,
+  changePassword,
+  getProfile,
+  updateProfilePicture,
+  updateCover,
+  updateDetails,
+} = require("../controllers/user");
+const { authUser } = require("../middlwares/auth");
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/activate", authUser, activateAccount);
+router.post("/login", login);
+router.post("/sendVerification", authUser, sendVerification);
+router.post("/findUser", findUser);
+router.post("/sendResetPasswordCode", sendResetPasswordCode);
+router.post("/validateResetCode", validateResetCode);
+router.post("/changePassword", changePassword);
+router.get("/getProfile/:username", authUser, getProfile);
+router.put("/updateProfilePicture", authUser, updateProfilePicture);
+router.put("/updateCover", authUser, updateCover);
+router.put("/updateDetails", authUser, updateDetails);
+
+module.exports = router;
